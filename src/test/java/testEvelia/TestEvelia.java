bd29f1b489ae408816d157f70e4113b7dff2d4da
Luchit0-afk <lucianopalacio91@gmail.com>
2021-10-27 09:14:44 -0300

    5to test de commit

--- src/test/java/testEvelia/TestEvelia.java
+++ src/test/java/testEvelia/TestEvelia.java
@@ -46,6 +46,7 @@ public class TestEvelia {
 		//System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
 		//File operaFile = new File("./drivers/operadriver");
 		//testtestTest
+		//test
 		//System.setProperty("webdriver.opera.driver", "/home/luchit0afk/eclipse-workspace/QuealityStreamTutorial/drivers/operadriver");
 		//System.setProperty("webdriver.edge.driver", "./drivers/msedgedriver");
 		System.setProperty("webdriver.opera.driver", "./drivers/operadriver");
