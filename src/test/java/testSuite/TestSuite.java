ab2bff85b90235bd3a273558bb1ce0610f9f9d82
Luciano Notebook <lucianopalacio@toptive.co>
2022-11-02 06:27:18 -0300

    Changes in testSuite to manage this class from docker

--- src/test/java/testSuite/TestSuite.java
+++ src/test/java/testSuite/TestSuite.java
@@ -1,7 +1,11 @@
 package testSuite;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,8 +16,9 @@ public class TestSuite {
 	@BeforeMethod
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		String remote_url_chrome = "http://localhost:4444/wd/hub";
+		ChromeOptions options = new ChromeOptions();
+		driver = new RemoteWebDriver(new URL(remote_url_chrome), options);
 		driver.get("https://server3.evelia.unrc.edu.ar/eveliajunit/index.jsp");
 	}
 
