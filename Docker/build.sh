85619a1ce620b28a3643cb7817b0d841f40ec580
Luchit0-afk <70291947+Luchit0-afk@users.noreply.github.com>
2022-12-13 20:31:50 -0300

    Update build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,3 +2,6 @@
 
 docker build -t "jmeter/docker:5.2.1" .
 docker pull selenium/standalone-chrome
+wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
+mv apache-maven-3.6.3 /opt/
